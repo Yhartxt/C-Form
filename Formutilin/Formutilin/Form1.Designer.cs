404c299490edffc25be9de748e34bd7591b28152
Yharim <gisel6ruz@gmail.com>
2025-03-04 22:26:30 -0600

    Add files via upload

---
+++ Formutilin/Formutilin/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Formutilin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Lbl_nombre = new System.Windows.Forms.Label();
+            this.Lbl_apellido = new System.Windows.Forms.Label();
+            this.Lbl_ifon = new System.Windows.Forms.Label();
+            this.LBL_Estaura = new System.Windows.Forms.Label();
+            this.Lbl_Edad = new System.Windows.Forms.Label();
+            this.Txt_nom = new System.Windows.Forms.TextBox();
+            this.Txt_Ape = new System.Windows.Forms.TextBox();
+            this.Txt_Tele = new System.Windows.Forms.TextBox();
+            this.Txt_Atura = new System.Windows.Forms.TextBox();
+            this.Txb_Edad = new System.Windows.Forms.TextBox();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.Btn_GUARDAR = new System.Windows.Forms.Button();
+            this.lbl_Genero = new System.Windows.Forms.GroupBox();
+            this.lbl_Femenino = new System.Windows.Forms.CheckBox();
+            this.lbl_masculino = new System.Windows.Forms.CheckBox();
+            this.lbl_Genero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Lbl_nombre
+            // 
+            this.Lbl_nombre.AutoSize = true;
+            this.Lbl_nombre.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_nombre.Location = new System.Drawing.Point(145, 99);
+            this.Lbl_nombre.Name = "Lbl_nombre";
+            this.Lbl_nombre.Size = new System.Drawing.Size(44, 13);
+            this.Lbl_nombre.TabIndex = 0;
+            this.Lbl_nombre.Text = "Nombre";
+            // 
+            // Lbl_apellido
+            // 
+            this.Lbl_apellido.AutoSize = true;
+            this.Lbl_apellido.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_apellido.Location = new System.Drawing.Point(145, 133);
+            this.Lbl_apellido.Name = "Lbl_apellido";
+            this.Lbl_apellido.Size = new System.Drawing.Size(44, 13);
+            this.Lbl_apellido.TabIndex = 1;
+            this.Lbl_apellido.Text = "Apellido";
+            // 
+            // Lbl_ifon
+            // 
+            this.Lbl_ifon.AutoSize = true;
+            this.Lbl_ifon.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_ifon.Location = new System.Drawing.Point(145, 171);
+            this.Lbl_ifon.Name = "Lbl_ifon";
+            this.Lbl_ifon.Size = new System.Drawing.Size(49, 13);
+            this.Lbl_ifon.TabIndex = 2;
+            this.Lbl_ifon.Text = "Telefono";
+            // 
+            // LBL_Estaura
+            // 
+            this.LBL_Estaura.AutoSize = true;
+            this.LBL_Estaura.BackColor = System.Drawing.Color.Transparent;
+            this.LBL_Estaura.Location = new System.Drawing.Point(145, 204);
+            this.LBL_Estaura.Name = "LBL_Estaura";
+            this.LBL_Estaura.Size = new System.Drawing.Size(46, 13);
+            this.LBL_Estaura.TabIndex = 3;
+            this.LBL_Estaura.Text = "Estatura";
+            // 
+            // Lbl_Edad
+            // 
+            this.Lbl_Edad.AutoSize = true;
+            this.Lbl_Edad.BackColor = System.Drawing.Color.Transparent;
+            this.Lbl_Edad.Location = new System.Drawing.Point(145, 239);
+            this.Lbl_Edad.Name = "Lbl_Edad";
+            this.Lbl_Edad.Size = new System.Drawing.Size(32, 13);
+            this.Lbl_Edad.TabIndex = 4;
+            this.Lbl_Edad.Text = "Edad";
+            // 
+            // Txt_nom
+            // 
+            this.Txt_nom.Location = new System.Drawing.Point(223, 92);
+            this.Txt_nom.Name = "Txt_nom";
+            this.Txt_nom.Size = new System.Drawing.Size(100, 20);
+            this.Txt_nom.TabIndex = 5;
+            // 
+            // Txt_Ape
+            // 
+            this.Txt_Ape.Location = new System.Drawing.Point(223, 130);
+            this.Txt_Ape.Name = "Txt_Ape";
+            this.Txt_Ape.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Ape.TabIndex = 6;
+            // 
+            // Txt_Tele
+            // 
+            this.Txt_Tele.Location = new System.Drawing.Point(223, 168);
+            this.Txt_Tele.Name = "Txt_Tele";
+            this.Txt_Tele.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Tele.TabIndex = 7;
+            // 
+            // Txt_Atura
+            // 
+            this.Txt_Atura.Location = new System.Drawing.Point(223, 204);
+            this.Txt_Atura.Name = "Txt_Atura";
+            this.Txt_Atura.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Atura.TabIndex = 8;
+            // 
+            // Txb_Edad
+            // 
+            this.Txb_Edad.Location = new System.Drawing.Point(223, 236);
+            this.Txb_Edad.Name = "Txb_Edad";
+            this.Txb_Edad.Size = new System.Drawing.Size(100, 20);
+            this.Txb_Edad.TabIndex = 9;
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_titulo.Location = new System.Drawing.Point(198, 39);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(100, 13);
+            this.lbl_titulo.TabIndex = 10;
+            this.lbl_titulo.Text = "Registro unach  tilin";
+            // 
+            // Btn_GUARDAR
+            // 
+            this.Btn_GUARDAR.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_GUARDAR.Location = new System.Drawing.Point(211, 368);
+            this.Btn_GUARDAR.Name = "Btn_GUARDAR";
+            this.Btn_GUARDAR.Size = new System.Drawing.Size(71, 23);
+            this.Btn_GUARDAR.TabIndex = 11;
+            this.Btn_GUARDAR.Text = "Guardar";
+            this.Btn_GUARDAR.UseVisualStyleBackColor = false;
+            this.Btn_GUARDAR.Click += new System.EventHandler(this.btn_sabe_Click);
+            // 
+            // lbl_Genero
+            // 
+            this.lbl_Genero.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Genero.Controls.Add(this.lbl_Femenino);
+            this.lbl_Genero.Controls.Add(this.lbl_masculino);
+            this.lbl_Genero.Location = new System.Drawing.Point(148, 262);
+            this.lbl_Genero.Name = "lbl_Genero";
+            this.lbl_Genero.Size = new System.Drawing.Size(200, 100);
+            this.lbl_Genero.TabIndex = 13;
+            this.lbl_Genero.TabStop = false;
+            this.lbl_Genero.Text = "Genero";
+            // 
+            // lbl_Femenino
+            // 
+            this.lbl_Femenino.AutoSize = true;
+            this.lbl_Femenino.Location = new System.Drawing.Point(107, 30);
+            this.lbl_Femenino.Name = "lbl_Femenino";
+            this.lbl_Femenino.Size = new System.Drawing.Size(72, 17);
+            this.lbl_Femenino.TabIndex = 15;
+            this.lbl_Femenino.Text = "Femenino";
+            this.lbl_Femenino.UseVisualStyleBackColor = true;
+            // 
+            // lbl_masculino
+            // 
+            this.lbl_masculino.AutoSize = true;
+            this.lbl_masculino.Location = new System.Drawing.Point(19, 30);
+            this.lbl_masculino.Name = "lbl_masculino";
+            this.lbl_masculino.Size = new System.Drawing.Size(69, 17);
+            this.lbl_masculino.TabIndex = 14;
+            this.lbl_masculino.Text = "Maculino";
+            this.lbl_masculino.UseVisualStyleBackColor = true;
+            this.lbl_masculino.CheckedChanged += new System.EventHandler(this.lbl_masculino_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 450);
+            this.Controls.Add(this.lbl_Genero);
+            this.Controls.Add(this.Btn_GUARDAR);
+            this.Controls.Add(this.lbl_titulo);
+            this.Controls.Add(this.Txb_Edad);
+            this.Controls.Add(this.Txt_Atura);
+            this.Controls.Add(this.Txt_Tele);
+            this.Controls.Add(this.Txt_Ape);
+            this.Controls.Add(this.Txt_nom);
+            this.Controls.Add(this.Lbl_Edad);
+            this.Controls.Add(this.LBL_Estaura);
+            this.Controls.Add(this.Lbl_ifon);
+            this.Controls.Add(this.Lbl_apellido);
+            this.Controls.Add(this.Lbl_nombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.lbl_Genero.ResumeLayout(false);
+            this.lbl_Genero.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_nombre;
+        private System.Windows.Forms.Label Lbl_apellido;
+        private System.Windows.Forms.Label Lbl_ifon;
+        private System.Windows.Forms.Label LBL_Estaura;
+        private System.Windows.Forms.Label Lbl_Edad;
+        private System.Windows.Forms.TextBox Txt_nom;
+        private System.Windows.Forms.TextBox Txt_Ape;
+        private System.Windows.Forms.TextBox Txt_Tele;
+        private System.Windows.Forms.TextBox Txt_Atura;
+        private System.Windows.Forms.TextBox Txb_Edad;
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Button Btn_GUARDAR;
+        private System.Windows.Forms.GroupBox lbl_Genero;
+        private System.Windows.Forms.CheckBox lbl_Femenino;
+        private System.Windows.Forms.CheckBox lbl_masculino;
+    }
+}
+
---
+++ Formutilin/Formutilin/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formutilin
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_masculino_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_sabe_Click(object sender, EventArgs e)
+        {
+            
+            string Apellido = Txt_Ape.Text;
+            string Nombre = Txt_nom.Text;
+
+            int Telfono = int.Parse(Txb_Edad.Text);
+            float Altrua = float.Parse(Txt_Atura.Text);
+            int Edad = int.Parse(Txb_Edad.Text);
+
+            string Genero = lbl_masculino.Checked ? "Masculino" : "Femenino";
+            string cnt = $"Nombre: {Nombre}\nApellido {Apellido}\nTelefono {Telfono}\nAltura {Altrua}\nGenero {Genero}";
+            string Archivo = @"D:\Users\gisel\Documents\xd.txt";
+
+            File.WriteAllText(Archivo, cnt);
+            MessageBox.Show("SE A ECHO EL ARCHIVO CON EXITO.");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
